e8ff2ef493f28c85d30e0972e67f4674f1c00bda
Ace <acearorong@sksu.edu.ph>
2025-02-17 02:21:55 +0800

    Update problem7.cpp

--- Problem set 1-9/problem7.cpp
+++ Problem set 1-9/problem7.cpp
@@ -2,20 +2,21 @@
 
 using namespace std;
 
-
 int main(){
 
-    string entries[3][3] = {{"Florence", "735-1234", "Manila"},
-       {"Joyce", "983-3333", "Quezon City"},
-       {"Becca", "456-3322", "Manila"}};
+    
+    string entry[3][3] = {{"Florence", "735-1234", "Manila"},
+                          {"Joyce", "983-3333", "Quezon City"},
+                          {"Becca", "456-3322", "Manila"}};
 
-       for(int i = 0; i < sizeof(entries) / sizeof(entries[0]); i++){
-        int b = 0;
-        cout << "Name: " << entries[i][b] << endl;
-        cout << "Tel.#: " << entries[i][b += 1] << endl;
-        cout << "Address: " << entries[i][b += 1] << endl;
-       }
+    for(int i = 0; i < 3; i++){
+        int x{0};
+        cout << "Name: " << entry[i][x++] << endl;
+        cout << "Address: " << entry[i][x++] <<endl;
+        cout << "Tel. #: " << entry[i][x] << endl;
+        cout << endl;
 
+    }
 
     return 0;
 }
