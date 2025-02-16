5240cbc7a191ea764b5a1de32ad3647d823676ed
Ace <acearorong@sksu.edu.ph>
2025-02-17 02:24:25 +0800

    Update problem8.cpp

--- Problem set 1-9/problem8.cpp
+++ Problem set 1-9/problem8.cpp
@@ -1,49 +1,39 @@
 #include <iostream>
-#include <vector>
-
-
+#include <algorithm>
 
 using namespace std;
 
+
 int main(){
 
-    vector<int> nums;
-    nums.resize(10);
 
-    for(int i = 0; i < nums.size(); i++){
-        cout << "Enter integer number: ";
-        cin >> nums[i];
-    }
-    cout << "Unsorted array elements: ";
-    for(int i = 0; i < nums.size(); i++){
-        cout << nums[i] << " ";
+    int intArr[10] = {9, 10, 8, 2, 7, 15, 6, 4, 5, 89};
+
+    cout << "================ Unsorted Array ================\n";
+
+    for(int y : intArr){
+        cout << y << " ";
     }
 
-    for(int i = 0; i < nums.size() - 1; i++){
-        
-        for(int j = 0; j < nums.size() - 1; j++){
-            if(nums[j] > nums[j + 1]){
-                int storage;
-                storage = nums[j];
-                nums[j] = nums[j + 1];
-                nums[j + 1] = storage;
-                
+
+    for(int j = 0; j < 10; j++){
+        bool swapped = false;
+        for(int i = 0; i < 10 - j - 1; i++){
+            if(intArr[i] > intArr[i + 1]){
+                swap(intArr[i], intArr[i + 1]);
+                swapped = true;
             }
-            
         }
-        
+        if(swapped == false){
+            break;
+        } 
     }
 
-    cout << endl;
+    cout << "\n\n================ Bubble Sorted ================\n";
 
-    cout << "Sorted array using Bubble Sort algorithm: ";
-
-    for(int i = 0; i < nums.size(); i++){
-        cout << nums[i] << " ";
+    for(int x:intArr){
+        cout << x << " ";
     }
-
-
-
 
     return 0;
 }
